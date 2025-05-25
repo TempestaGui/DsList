0a1649c27d0498c97bdc49116c8f2511f9d0a903
DevGuilhermeTempesta <guilhermeftempesta@gmail.com>
2025-05-25 12:13:02 -0300

    mover games na lista

--- src/main/java/com/Projeto/DsList/service/GameListService.java
+++ src/main/java/com/Projeto/DsList/service/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.Projeto.DsList.dto.GameListDTO;
 import com.Projeto.DsList.entities.GameList;
+import com.Projeto.DsList.projections.GameMinProjection;
 import com.Projeto.DsList.repositories.GameListRepository;
+import com.Projeto.DsList.repositories.GameRepository;
 
 //registrar um componente, injetar um componente no outro
 @Service	
@@ -17,6 +19,8 @@ public class GameListService {
 	//injetando uma instancia do gameRepository no gameService
 	@Autowired
 	private GameListRepository gameListRepository;
+	@Autowired
+	private GameRepository gameRepository;
 	//garante o ACID
 	
 	@Transactional(readOnly = true)
@@ -26,5 +30,20 @@ public class GameListService {
 		//operaçao para transformar uma lista games numa lista de gamesmin
 		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 		return dto;
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId); 
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex; 
+		
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex; 
+		
+		for(int i = min; i<max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
